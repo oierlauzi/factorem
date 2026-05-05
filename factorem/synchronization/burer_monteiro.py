642461e8fe06c9d7c5bf29a262f7a44256ff9c4e
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-05-05 09:32:01 +0200

    Rename

--- factorem/synchronization/burer_monteiro.py
+++ factorem/synchronization/burer_monteiro.py
@@ -8,11 +8,11 @@ def _orthogonalize_matrices(matrices: np.ndarray, special: bool) -> np.ndarray:
     return result
 
 def _optimization_step(
-    samples: scipy.sparse.bsr_matrix,
+    similarities: scipy.sparse.bsr_matrix,
     transforms: np.ndarray,
     special: bool
 ) -> np.ndarray:
-    result = samples @ transforms.reshape(-1, transforms.shape[-1])
+    result = similarities @ transforms.reshape(-1, transforms.shape[-1])
     result = result.reshape(transforms.shape)
     result = _orthogonalize_matrices(result, special=special)
     return result
@@ -39,7 +39,7 @@ def burer_monteiro_random_start(
     return start
 
 def burer_monteiro_ortho_group_synchronization(
-    samples: scipy.sparse.bsr_matrix,
+    similarities: scipy.sparse.bsr_matrix,
     start: np.ndarray,
     special: bool = False,
     tol: float = 1e-8,
@@ -49,7 +49,7 @@ def burer_monteiro_ortho_group_synchronization(
     for _ in range(max_iter):
         prev_x = x
         x = _optimization_step(
-            samples=samples,
+            similarities=similarities,
             transforms=x,
             special=special
         )
