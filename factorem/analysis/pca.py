60ef5c57b7a460577eec9769daf82f40195fc6a3
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-05-31 14:49:01 +0200

    Performing PCA on unpadded images

--- factorem/analysis/pca.py
+++ factorem/analysis/pca.py
@@ -13,12 +13,20 @@ def _mean_center(
     mean = x.mean(axis=0, where=valid)
     return x - mean
 
+def _remove_padding(
+    images: jax.Array,
+    particle_size: int
+) -> jax.Array:
+    return images[:,:particle_size,:particle_size]
+    
 class PCA(Processor):
     def __init__(
         self,
-        n_components: int
+        n_components: int,
+        particle_size: int
     ):
         self.n_components = n_components
+        self.particle_size = particle_size
 
     def fit_transform(
         self,
@@ -28,6 +36,10 @@ class PCA(Processor):
     ) -> jax.Array:
         n_padded = images.shape[0]
         wiener_corrected_images = wiener_ctf_correct_2d(images, ctfs)
+        wiener_corrected_images = _remove_padding(
+            wiener_corrected_images, 
+            self.particle_size
+        )
         
         valid = jnp.arange(n_padded) < count
         x = wiener_corrected_images.reshape(len(wiener_corrected_images), -1)
